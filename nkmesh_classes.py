a065c7d9738c908ffda7868e6eb9e4f5f26f923a
polavux <polavux@proton.me>
2026-09-01 07:37:17 -0400

    Update nkmesh_classes.py
    added and corrected a few things, so now this works on all 1000ish nkmesh files i tested

--- nkmesh_classes.py
+++ nkmesh_classes.py
@@ -13,7 +13,7 @@ def unpack_next_single_int(data) -> int:
 
 def unpack_next_string(data) -> str:
     length = unpack_next_single_int(data)
-    return unpack_next(f"{length}s", data)[0].decode("ascii")
+    return unpack_next(f"{length}s", data)[0].decode("utf-8")
 
 def unpack_next_array(format : str, data, length : int, struct_size : int = 1) -> tuple:
     return tuple(unpack_next(format, data) for _ in range(length // struct_size))
@@ -39,17 +39,20 @@ class Nkmesh:
         with open(file_path, "rb") as data:
             version = unpack_next_single_int(data)
             
-            print(f"reading {file_path} version {version}")
+            print(f"reading {file_path} (version: {version})")
 
             num_nodes = unpack_next_single_int(data)
             nodes = tuple(self.NkmeshNode.from_stream(data) for _ in range(num_nodes))
             
             num_meshes = unpack_next_single_int(data)
             meshes = tuple(self.NkmeshMesh.from_stream(data) for _ in range(num_meshes))
-            
-            # todo: rest of the file
 
-        return self(version, nodes, meshes)
+            # todo: figure out the stuff that sometimes appears between these
+            
+            #num_anims = unpack_next_single_int(data)
+            #anims = tuple(self.NkmeshAnim.from_stream(data) for _ in range(num_anims))
+
+        return self(version, nodes, meshes, None) #return self(version, nodes, meshes, anims)
 
 
 
@@ -60,7 +63,7 @@ class Nkmesh:
 
             name = unpack_next_string(data)
             texture = unpack_next_string(data)
-            unknown_4_bytes = unpack_next_single_int(data)
+            unknown_4_bytes = unpack_next_single_int(data) # might be a unix timestamp?
 
             print(f"Found mesh |name: {name:<50}|texture name: {texture:<50}|unknown bytes: {unknown_4_bytes}")
 
@@ -73,25 +76,26 @@ class Nkmesh:
             polygons            = unpack_next_array("<III", data, num_polygons // 3)
             vert_coords         = unpack_next_array("<fff", data, num_uv_coords)
             vert_normals        = unpack_next_array("<fff", data, num_uv_coords)
-            unknown_flag        = unpack_next("<b", data)
-            num_unknown_values  = unpack_next_single_int(data)
-            unknown_values      = unpack_next_rle("<ffffff", data, num_unknown_values, 6)
+            unknown_flag,       = unpack_next("<B", data)
+
+            if unknown_flag:
+                num_unknown_values  = unpack_next_single_int(data)
+                unknown_values      = unpack_next_rle("<ffffff", data, num_unknown_values, 6)
 
             return self(
                 name, texture, unknown_4_bytes,
                 vert_coords, vert_normals, vert_color_sets, uv_coords, polygons
             )
 
-        
+
 
         # temporary, may replace with a proper NkmeshVertex class later
         @dataclass
         class NkmeshVertexColorSet:
             @classmethod
             def from_stream(self, data):
-                # colors use run-length encoding
                 num_colors = unpack_next_single_int(data)
-                num_colors2 = unpack_next_single_int(data)
+                num_colors_dupe = unpack_next_single_int(data)
 
                 return self(unpack_next_rle("<BBB", data, num_colors, 3))
 
@@ -118,16 +122,20 @@ class Nkmesh:
             name = unpack_next_string(data)
             node_type = unpack_next_string(data)            
 
-            parent_node_index, mesh_index, grandparent_node_index, num_child_nodes, unknown_int = unpack_next("<iiiII", data)
+            parent_node_index, mesh_index, grandparent_node_index, num_child_nodes, unknown_int_always_zero = unpack_next("<iiiII", data)
             child_nodes = unpack_next(f"<{num_child_nodes}I", data)
-            unknown_transforms, unknown_4_bytes, offset_next_node = unpack_next("<41f", data), unpack_next_single_int(data), unpack_next_single_int(data)
+
+            unknown_values = unpack_next("<41f", data)
+            unknown_flag_1, unknown_flag_2, unknown_flag_3, unknown_flag_4 = unpack_next("<BBBB", data)
+            offset_next_node = unpack_next_single_int(data)
             
-            print(f"Found node |name: {name:<50}|type: {node_type:<50} {unknown_int} {unknown_4_bytes}")
+            print(f"Found node |name: {name:<50}|type: {node_type:<50}|flags: {unknown_flag_1} {unknown_flag_2} {unknown_flag_3} {unknown_flag_4}")
 
             return self(
                 name, node_type,
-                parent_node_index, mesh_index, grandparent_node_index, child_nodes, unknown_int,
-                 unknown_transforms, unknown_4_bytes, offset_next_node
+                parent_node_index, mesh_index, grandparent_node_index, child_nodes,
+                unknown_values, unknown_int_always_zero, unknown_flag_1, unknown_flag_2, unknown_flag_3, unknown_flag_4,
+                offset_next_node
             )
 
         # NkmeshNode members
@@ -137,12 +145,37 @@ class Nkmesh:
         mesh_index: int
         grandparent_node_index: int
         child_nodes: tuple[int]
-        unknown_int: int
-        unknown_transforms: tuple[float]
-        unknown_4_bytes: int
+        unknown_values: tuple[float]
+        unknown_int_always_zero: int
+        unknown_flag_1: int
+        unknown_flag_2: int
+        unknown_flag_3: int
+        unknown_flag_4: int
         offset_next_node: int
+
+
+
+    @dataclass
+    class NkmeshAnim:
+        @classmethod
+        def from_stream(self, data):
+            unknown_double, = unpack_next("<d", data)
+            name = unpack_next_string(data)
+            index = unpack_next_single_int(data)
+
+            print(unknown_double, name, index)
+
+            return self(
+                name, unknown_double, index
+            )
+
+        # NkmeshAnim members
+        name: str
+        unknown_double: float
+        index: int
 
     # Nkmesh members
     version: int
     nodes: tuple[NkmeshNode]
     meshes: tuple[NkmeshMesh]
+    anims: tuple[NkmeshAnim]
